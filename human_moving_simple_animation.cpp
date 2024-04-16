fc26d54f908a296880f3c5ece9f09ea4dbfb4199
andrei-necsulea <83091934+andrei-necsulea@users.noreply.github.com>
2024-04-17 00:47:21 +0300

    Add files via upload

---
+++ human_moving_simple_animation.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<stdlib.h>
+#include<stdio.h>
+using namespace std;
+int main(){
+    int i,j,k;
+    for(i=1;i<=15;i++){
+            system("cls");
+      for(j=1;j<=15/2;j++){
+    cout<<" *";
+    cout<<"     *"<<endl;
+    cout<<"  * ";
+    cout<<"O"<<" *"<<endl<<"    *"<<endl<<"    *"<<endl<<"    *"<<endl;
+    cout<<"   *";
+    cout<<" *"<<endl;
+    cout<<" *    *";
+    cout<<endl;
+    cout<<endl;
+    system("cls");
+
+      }
+    for(k=1;k<=15/2;k++){
+            system("cls");
+    cout<<"    O"<<endl<<"  * * *"<<endl<<" *  *  *"<<endl<<"    *"<<endl;
+    cout<<"   *";
+    cout<<" *"<<endl;
+    cout<<" *    *";
+    }
+    }
+    system("cls");
+}
